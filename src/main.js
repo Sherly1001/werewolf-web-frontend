b693d89a9f6745caedc9cc5f826b51a4230879f9
N0tail12 <N0tail12>
2021-11-19 02:27:02 +0700

    add authorize, services

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { install as DiscordMessageComponents } from "@discord-message-components/vue";
-import VueSocketIO from "vue-3-socket.io";
 import VueCookies from "vue3-cookies";
 import "@discord-message-components/vue/dist/style.css";
 import dotenv from "dotenv";
@@ -14,12 +13,6 @@ dotenv.config();
 
 library.add(fas);
 createApp(App)
-  .use(
-    new VueSocketIO({
-      debug: true,
-      connection: "",
-    })
-  )
   .use(router)
   .use(VueCookies)
   .use(DiscordMessageComponents)
