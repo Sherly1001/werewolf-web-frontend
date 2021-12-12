bc7238630ba6e5b751b78a471bf763052244e16c
N0tail12 <N0tail12>
2021-12-13 02:22:31 +0700

    fix: scroll-bar and online/offline

--- src/views/MainSite.vue
+++ src/views/MainSite.vue
@@ -5,7 +5,7 @@
       <router-view :info="info" :messagesRecv="messages" />
       <InputBar :emitSend="onSendMsg" />
     </div>
-    <Member :online="online" :offline="offline" />
+    <Member :online="Object.values(online)" :offline="Object.values(offline)" />
   </div>
 </template>
 
@@ -29,8 +29,8 @@ export default {
       messages: [],
       channel_id: "1",
       message: "",
-      online: [],
-      offline: [],
+      online: {},
+      offline: {},
       position: 0,
     };
   },
@@ -67,26 +67,26 @@ export default {
       let data = JSON.parse(m.data);
       if (data.GetUsersRes) {
         this.users = data.GetUsersRes;
-        this.online = this.users.filter((user) => user.is_online);
-        this.offline = this.users.filter((user) => !user.is_online);
+        this.users
+          .filter((user) => user.is_online)
+          .forEach((u) => {
+            this.online[u.id] = u;
+          });
+        this.users
+          .filter((user) => !user.is_online)
+          .forEach((u) => {
+            this.offline[u.id] = u;
+          });
       }
       if (data.UserOnline) {
         data.UserOnline.is_online = true;
-        this.position = user.containsObject(data.UserOnline, this.online);
-        if (this.position == -1) {
-          this.online.push(data.UserOnline);
-          let find = user.containsObject(data.UserOnline, this.offline);
-          this.offline.splice(find, 1);
-        }
+        this.online[data.UserOnline.id] = data.UserOnline;
+        delete this.offline[data.UserOnline.id];
       }
       if (data.UserOffline) {
         data.UserOffline.is_online = false;
-        this.position = user.containsObject(data.UserOffline, this.offline);
-        if (this.position == -1) {
-          this.offline.push(data.UserOffline);
-          let find = user.containsObject(data.UserOffline, this.online);
-          this.online.splice(find, 1);
-        }
+        this.offline[data.UserOffline.id] = data.UserOffline;
+        delete this.online[data.UserOffline.id];
       }
       if (data.GetMsgRes) {
         this.messages = recv.getAllMessages(
